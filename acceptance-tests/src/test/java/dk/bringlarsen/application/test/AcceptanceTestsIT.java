c15c679242553b65e7c7d52747afa9e49e23a3a9
jla <jessbringlarsen@gmail.com>
2023-08-31 11:28:03 +0200

    Use testcontainers to spin up postgres

--- acceptance-tests/src/test/java/dk/bringlarsen/application/test/AcceptanceTestsIT.java
+++ acceptance-tests/src/test/java/dk/bringlarsen/application/test/AcceptanceTestsIT.java
@@ -1,10 +1,24 @@
 package dk.bringlarsen.application.test;
 
+import io.cucumber.java.AfterAll;
+import io.cucumber.java.BeforeAll;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @Suite
 @SelectClasspathResource("dk/bringlarsen/application/test")
 public class AcceptanceTestsIT {
 
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine");
+
+    @BeforeAll
+    static void beforeAll() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
 }
